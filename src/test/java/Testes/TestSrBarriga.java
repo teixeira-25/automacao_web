6869c8e6885f254d3f2076775b6ed2d4deccc92b
Mick Hill - MacBook Pro M1 <mickhill@hotmail.com.br>
2024-10-22 18:07:58 -0300

    Adicionando packer de driveres para o navegador web

--- src/test/java/Testes/TestSrBarriga.java
+++ src/test/java/Testes/TestSrBarriga.java
@@ -1,5 +1,6 @@
 package Testes;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -9,6 +10,7 @@ public class TestSrBarriga
     @Test
     public void teste() throws InterruptedException
     {
+        WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
         driver.get("https://seubarriga.wcaquino.me");
         Thread.sleep(3000);
