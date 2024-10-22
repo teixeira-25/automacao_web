fdaaeea8e292f3e51a9e40cfad176887e542e73f
Mick Hill - MacBook Pro M1 <mickhill@hotmail.com.br>
2024-10-22 18:02:07 -0300

    Chamada do navegado com webdriver puro!

--- src/test/java/Testes/TestSrBarriga.java
+++ src/test/java/Testes/TestSrBarriga.java
@@ -1,12 +1,18 @@
 package Testes;
 
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class TestSrBarriga
 {
     @Test
-    public void teste()
+    public void teste() throws InterruptedException
     {
-        
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://seubarriga.wcaquino.me");
+        Thread.sleep(3000);
+        driver.quit();
+        driver = null;
     }
 }
